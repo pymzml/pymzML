fd803874b7c3b8fb38bed7988eece3b66d909a49
Hannes Roest <roest@imsb.biol.ethz.ch>
2013-04-25 12:26:04 +0200

    [FEATURE] add indexed reading also for non-indexed files
    - pre-read the files and store the binary indices of all occurences of
      chromatogram / spectra objects

--- src/run.py
+++ src/run.py
@@ -93,7 +93,8 @@ class Reader(object):
                      noiseThreshold = 0.0,
                      extraAccessions = None,
                      MS1_Precision = 5e-6,
-                     MSn_Precision = 20e-6
+                     MSn_Precision = 20e-6,
+                     build_index_from_scratch=False
         ):
 
 
@@ -167,6 +168,8 @@ class Reader(object):
             if self.info['offsets']['indexList'] == None:
                 # fall back to non-seekable
                 self.info['seekable'] = False
+                if build_index_from_scratch:
+                    self._build_index_from_scratch(self.seeker)
                 # print('Could not find indexList. Falling back to non seekable.', file = sys.stderr)
             elif self.info['offsets']['TIC']  > os.path.getsize(self.info['filename']):
                 self.info['seekable'] = False
@@ -239,6 +242,80 @@ class Reader(object):
                         self.param['accessions'][accession]['valuesToExtract'].append(valueToExtract)
 
         return
+
+    def _build_index_from_scratch(self, seeker):
+
+        def get_data_indices(fh, chunksize=8192, lookback_size=100):
+            """ Get a dictionary with binary file indices of spectra and chromatograms in an mzML file.
+
+            Will parse quickly through the file and find all occurences of 
+            <chromatogram ... id="..." and <spectrum ... id="..." using a
+            regex.
+            We dont use an XML parser here because we need to know the
+            exact location of the filepointer which is usually not possible
+            with common xml parsers.
+            """
+            chrom_positions = {}
+            spec_positions = {}
+            chromcnt = 0
+            speccnt = 0
+            # regexes to be used
+            import re 
+            chromexp = re.compile("<\s*chromatogram[^>]*id=\"([^\"]*)\"")
+            chromcntexp = re.compile("<\s*chromatogramList\s*count=\"([^\"]*)\"")
+            specexp = re.compile("<\s*spectrum[^>]*id=\"([^\"]*)\"")
+            speccntexp = re.compile("<\s*spectrumList\s*count=\"([^\"]*)\"")
+            # go to start of file 
+            fh.seek(0)
+            prev_chunk = ""
+            while True:
+                 # read a chunk of data
+                offset = fh.tell()
+                chunk = fh.read(chunksize)
+                if not chunk: break
+
+                 # append a part of the previous chunk since we have cut in the middle
+                 # of the text (to make sure we dont miss anything, prev_chunk
+                 # is analyzed twice).
+                if len(prev_chunk) > 0:
+                    chunk = prev_chunk[-lookback_size:] + chunk
+                    offset -= lookback_size
+                prev_chunk = chunk
+
+                # find all occurences of the expressions and add to the dictionary
+                for m in chromexp.finditer(chunk):
+                    chrom_positions[m.group(1)] = offset + m.start()
+                for m in specexp.finditer(chunk):
+                    spec_positions[m.group(1)] = offset + m.start()
+                m = chromcntexp.search(chunk)
+
+                # also look for the total count of chromatograms and spectra 
+                # -> must be the same as the content of our dict!
+                if m is not None:
+                    chromcnt = int(m.group(1))
+                m = speccntexp.search(chunk)
+                if m is not None:
+                    speccnt = int(m.group(1))
+
+            # Check if everything is ok (e.g. we found the right number of
+            # chromatograms and spectra) and then return the dictionary.
+            if (chromcnt == len(chrom_positions) and
+                speccnt == len(spec_positions)  ):
+                positions = {}
+                positions.update(chrom_positions)
+                positions.update(spec_positions)
+                return positions
+            else: return None
+
+        indices = get_data_indices(seeker)
+        if indices is None: return
+
+        self.info['offsets'].update(indices) 
+        self.info['offsetList'].extend(indices.values())
+
+        # make sure the list is sorted (for bisect)
+        self.info['offsetList'] = sorted( self.info['offsetList'] )
+        self.info['seekable'] = True
 
     def __iter__(self):
         return self
