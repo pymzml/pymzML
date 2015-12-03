3f8ac4d16a6859c89dfd63ad8b76bf0943cc4292
Johannes Barth <johannesbarth@uni-muenster.de>
2015-12-03 13:19:20 +0100

    Fixed test_deconvolution

--- test/test_readChromMzML.py
+++ test/test_readChromMzML.py
@@ -71,7 +71,10 @@ class TestReadChromatogram(unittest.TestCase):
 
     @attr('numpress')
     def test_read_numpress(self):
-        run = pymzml.run.Reader(self.mini_numpress_file)
+        run = pymzml.run.Reader(
+            self.mini_numpress_file,
+            obo_version = '1.1.0'
+        )
         self.assertFalse(run.info['seekable'])
 
         all_chromatograms = list(run)
