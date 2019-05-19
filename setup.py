9a6f87e29ff542c37c3a87bb69663895f5acaccd
Manuel <m_koes17@uni-muenster.de>
2019-05-19 15:52:27 +0200

    Update setup.py and requirements file
    - remove version specification from regex in requirements file and
    setup.py
    - stop testing formatting with tox

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     python_requires  = '>=3.4.0',
     install_requires = [
         'numpy >= 1.8.0',
-        'regex>2019.4.14'
+        'regex'
     ],
     extras_require   = {
         'full': [
