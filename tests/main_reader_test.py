e04b65239bb6b18aa7ff581e18f654eca77586d8
Manuel <m.koes17@gmail.com>
2019-11-15 17:05:16 +0100

    Test setting of measured precision
    - Add test to ensure setting MS_precisions dict in reader propagtes to
    returned spectra

--- tests/main_reader_test.py
+++ tests/main_reader_test.py
@@ -45,6 +45,10 @@ class runTest(unittest.TestCase):
         with run.Reader(self.paths[0]) as reader:
             reader[2]
 
+    def test_setting_measured_precision(self):
+        reader = run.Reader(self.paths[2], MS_precisions={1: 61})
+        spec = reader[1]
+        self.assertEqual(spec.measured_precision, 61)
 
     def test_determine_file_encoding(self):
         """
